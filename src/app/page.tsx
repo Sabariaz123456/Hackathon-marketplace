9bfef36fc32079d9141ce4fd85b45b8a277c882c
Saba Muhammad Riaz <sabaranicool94@gmail.com>
2025-02-04 11:10:37 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "../app/components/hero2"
 import NewArrwa from "../app/components/NewArrwa"
 import Hero2 from "./Hero2/page"
 import Toppicks from "../app/components/TopPicks"
-import Whatshopping from "../app/components/whatshopping"
+import Whatshopping from "../app/components/Ourblog"
 export default function Home() {
   return (
     <div >
